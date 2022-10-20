57d52db659196ec2a9ec61ad574bd0cc83a46841
Tigst Demisse <tdemisse123@gmail.com>
2022-10-20 15:50:36 -0500

    removing unused libraries

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config();
-const employeeRoute = require('./routes/employee')
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const employeeRoute = require("./routes/employee");
 const cors = require("cors");
-
 
 const app = express();
 
-mongoose.connect(process.env.MONGODB_URI).then(()=>{
-    console.log("database connection is sucessful")
-}).catch((err)=>{
-    console.log(err)
-})
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("database connection is sucessful");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.use(express.json());
 app.use(cors({ origin: "*" }));
-app.use("/api/employees", employeeRoute)
-
-
+app.use("/api/employees", employeeRoute);
 
 app.listen(5000, () => {
-    console.log("our server is up and running port 5000");
-})
+  console.log("the server is up and running on port 5000");
+});
